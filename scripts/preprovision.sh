d80d9a894ea6dd514fa90ceb49ea80969d3f12b2
jmservera <8036360+jmservera@users.noreply.github.com>
2025-10-24 11:38:41 +0200

    Refactor Azure deployment scripts: remove PowerShell scripts and enhance preprovision.sh for OAUTH settings management

--- scripts/preprovision.sh
+++ scripts/preprovision.sh
@@ -6,10 +6,28 @@ if [ -z "$GITHUB_SECRET" ]; then
     azd env set CLIENT_IP_ADDRESS $(curl ifconfig.me 2>/dev/null | tr -d '\r')
 fi
 
+WEB_APP_NAME=$(azd env get-value WEB_APP_NAME)
+RESOURCE_GROUP_NAME=$(azd env get-value RESOURCE_GROUP_NAME)
 if [ -n "$WEB_APP_NAME" ]; then
     echo "Fetching OAUTH_ settings from Web App: $WEB_APP_NAME ..."
     oauth_values=$(az webapp config appsettings list -n $WEB_APP_NAME -g $RESOURCE_GROUP_NAME --query "[] | [? contains(name,'OAUTH_')]")
-    azd env set OAUTH_SETTINGS "$oauth_values"
+    oauth_values_staging=$(az webapp config appsettings list -n $WEB_APP_NAME -g $RESOURCE_GROUP_NAME --slot staging --query "[] | [? contains(name,'OAUTH_')]")
+    oauth_values=$(echo "$oauth_values" | tr -d '\r\n')
+    oauth_values_staging=$(echo "$oauth_values_staging" | tr -d '\r\n')
+
+    if [ -n "$oauth_values" ]; then
+        echo "OAUTH_ settings fetched for production slot."
+        azd env set OAUTH_SETTINGS "$oauth_values"
+    else
+        echo "No OAUTH_ settings found for production slot."
+    fi
+
+    if [ -n "$oauth_values_staging" ]; then
+        echo "OAUTH_ settings fetched for staging slot."
+        azd env set OAUTH_SETTINGS_STAGING "$oauth_values_staging"
+    else
+        echo "No OAUTH_ settings found for staging slot."
+    fi
 else
     echo "WEB_APP_NAME is not set. Skipping fetching OAUTH_ settings."
 fi
