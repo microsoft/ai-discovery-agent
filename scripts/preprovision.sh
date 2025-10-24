3ef4788aa289096192295e27fd4cbee20e445e02
jmservera <8036360+jmservera@users.noreply.github.com>
2025-10-24 10:32:33 +0200

    Enhance configuration and scripts for improved debugging and OAuth settings
    - Updated launch.json to specify Python interpreter path for debugging.
    - Refined settings.json for isort and bandit configurations.
    - Added LOG_LEVEL parameter in resources.bicep for better logging control.
    - Enhanced preprovision.sh to fetch OAUTH_ settings from Azure Web App if WEB_APP_NAME is set.
    - Adjusted .example.env to remove unnecessary space in AZURE_STORAGE_CONNECTION_STRING.
    - Modified startup.sh to include OAUTH_ in environment variable display and added dynamic HOST and LOG_LEVEL settings.

--- scripts/preprovision.sh
+++ scripts/preprovision.sh
@@ -6,5 +6,14 @@ if [ -z "$GITHUB_SECRET" ]; then
     azd env set CLIENT_IP_ADDRESS $(curl ifconfig.me 2>/dev/null | tr -d '\r')
 fi
 
+if [ -nz "$WEB_APP_NAME" ]; then
+    echo "Fetching OAUTH_ settings from Web App: $WEB_APP_NAME ..."
+    oauth_values=$(az webapp config appsettings list -n $WEB_APP_NAME -g $RESOURCE_GROUP_NAME --query "[] | [? contains(name,'OAUTH_')]")
+    azd env set OAUTH_SETTINGS "$oauth_values"
+else
+    echo "WEB_APP_NAME is not set. Skipping fetching OAUTH_ settings."
+fi
+
+
 repo_name=$(gh repo view --json nameWithOwner -t '{{.nameWithOwner}}')
 azd env set GITHUB_REPOSITORY "$repo_name"
