0118737f2c776cac74b77b2452eef81a009ff370
jmservera <8036360+jmservera@users.noreply.github.com>
2025-11-12 18:16:13 +0100

    Update Mermaid script integrity and add referrer policy for security

--- src/aida/static/elements/MermaidViewer.jsx
+++ src/aida/static/elements/MermaidViewer.jsx
@@ -7,11 +7,13 @@ export default function MermaidViewer(props) {
   useEffect(() => {
     if (!window.mermaid) {
       const script = document.createElement("script");
+
       script.src =
         "https://cdnjs.cloudflare.com/ajax/libs/mermaid/9.3.0/mermaid.min.js";
       script.async = true;
-      script.integrity = "sha384-FvYCmr1hpAaiRMsY27v6YiuuQqZf5yyUMrMMbF92WcB7xwypTn903s9Fq2N36osG";
+      script.integrity = "sha512-ku2nmBrzAXY5YwohzTqLYH1/lvyMrpTVxgQKrvTabd/b/uesqltLORdmpVapYv6QhZVCLUX6wkvFaKOAY4xpUA==";
       script.crossOrigin = "anonymous";
+      script.referrerPolicy = "no-referrer";
       document.body.appendChild(script);
       script.onload = () => {
         window.mermaid.initialize({
