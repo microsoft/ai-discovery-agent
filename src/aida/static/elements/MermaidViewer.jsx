6359dd045137efed04254579ada6d36856580550
jmservera <8036360+jmservera@users.noreply.github.com>
2025-11-12 18:30:42 +0100

    Update Mermaid script version and remove unused props parameter

--- src/aida/static/elements/MermaidViewer.jsx
+++ src/aida/static/elements/MermaidViewer.jsx
@@ -2,16 +2,16 @@
 // Licensed under the MIT license.
 
 import { useEffect } from "react";
+props;
 
-export default function MermaidViewer(props) {
+export default function MermaidViewer() {
   useEffect(() => {
     if (!window.mermaid) {
       const script = document.createElement("script");
-
       script.src =
-        "https://cdnjs.cloudflare.com/ajax/libs/mermaid/9.3.0/mermaid.min.js";
+        "https://cdnjs.cloudflare.com/ajax/libs/mermaid/11.12.0/mermaid.min.js";
       script.async = true;
-      script.integrity = "sha512-ku2nmBrzAXY5YwohzTqLYH1/lvyMrpTVxgQKrvTabd/b/uesqltLORdmpVapYv6QhZVCLUX6wkvFaKOAY4xpUA==";
+      script.integrity = "sha512-5TKaYvhenABhlGIKSxAWLFJBZCSQw7HTV7aL1dJcBokM/+3PNtfgJFlv8E6Us/B1VMlQ4u8sPzjudL9TEQ06ww==";
       script.crossOrigin = "anonymous";
       script.referrerPolicy = "no-referrer";
       document.body.appendChild(script);
